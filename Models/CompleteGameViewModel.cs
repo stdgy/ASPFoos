36036df04014908bc9280c188abd9daf3bb00904
Daniel Mathis <daniel.t.mathis@gmail.com>
2017-05-27 19:11:01 -0700

    Added completed game edit screen.

---
+++ Models/CompleteGameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace foosball_asp.Models
+{
+    public class CompleteGameViewModel
+    {
+        public int GameId { get; set; }
+        public TeamViewModel RedTeam { get; set; }
+        public TeamViewModel BlueTeam { get; set; }
+
+        public List<ScoreViewModel> Scores { get; set; }
+    }
+}
